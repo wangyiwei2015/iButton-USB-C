f0ded171af6cc342bfa9494a41b25fadb04c5001
wangyiwei <wangyw.dev@outlook.com>
2024-03-31 20:34:39 +0800

    added stlink edge 3

--- iButton-FW/cmake-build-debug/cmake_install.cmake
+++ iButton-FW/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/wangyiwei/CLionProjects/STM32_USB_Test
+# Install script for directory: C:/Users/wangyiwei/Documents/GitHub/iButton-USB-C/iButton-FW
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/wangyiwei/CLionProjects/STM32_USB_Test/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/wangyiwei/Documents/GitHub/iButton-USB-C/iButton-FW/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
