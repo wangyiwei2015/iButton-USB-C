22dd2bcae917ce1e293e19b25a1c8b014aa85511
wangyiwei <wangyw.dev@outlook.com>
2024-04-15 02:37:22 +0800

    changed codes

--- iButton-FW1/USB_DEVICE/App/usbd_desc.c
+++ iButton-FW1/USB_DEVICE/App/usbd_desc.c
@@ -64,9 +64,9 @@
 
 #define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
+#define USBD_MANUFACTURER_STRING     "STM-DIY"
 #define USBD_PID_FS     22315
-#define USBD_PRODUCT_STRING_FS     "STM32 Human interface"
+#define USBD_PRODUCT_STRING_FS     "iButton-Gen1"
 #define USBD_CONFIGURATION_STRING_FS     "HID Config"
 #define USBD_INTERFACE_STRING_FS     "HID Interface"
 
